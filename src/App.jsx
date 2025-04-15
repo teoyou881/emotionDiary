07053a5809baf57cf418c6bea60b64d49a83f313
Teo_You <teoyou881@gmail.com>
2025-04-15 15:50:27 -0400

    emotionDiary - font, image, ui set

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home.jsx';
+import New from './pages/New.jsx';
+import Diary from './pages/Diary.jsx';
+import {Link, Route, Routes, useNavigate} from 'react-router-dom';
+import Notfound from './pages/Notfound.jsx';
 
+import {getEmotionImage} from './util/get-emotion-image.js';
+
+// 1. "/" : Home
+// 2. "/new" : New
+// 3. "/diary" : Diary
 function App() {
-  const [count, setCount] = useState(0)
 
+  let nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new")
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+        <img src={getEmotionImage(1)} alt=""/>
+        <img src={getEmotionImage(2)} alt=""/>
+        <img src={getEmotionImage(3)} alt=""/>
+        <img src={getEmotionImage(4)} alt=""/>
+        <img src={getEmotionImage(5)} alt=""/>
+        <div>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/new'}>New</Link>
+          <Link to={'/diary'}>Diary</Link>
+        </div>
+        <button onClick={onClickButton}> go to new page</button>
+        <Routes>
+          <Route path={'/'} element={<Home/>}></Route>
+          <Route path={'/new'} element={<New/>}></Route>
+          <Route path={'/diary/:id'} element={<Diary/>}></Route>
+          <Route path="*" element={<Notfound/>}></Route>
+        </Routes>
+      </>
   )
 }
 
