1d25a8fe7a2e9386aaf6eed759997dab2faf2c08
Teo_You <teoyou881@gmail.com>
2025-04-15 22:18:02 -0400

    emotionDiary - onCreate, onUpdate, onDelete function

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,107 @@ import './App.css'
 import Home from './pages/Home.jsx';
 import New from './pages/New.jsx';
 import Diary from './pages/Diary.jsx';
-import {Link, Route, Routes, useNavigate} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
 import Notfound from './pages/Notfound.jsx';
 import Button from './components/Button.jsx';
 import Header from './components/Header.jsx';
+import Edit from './pages/Edit.jsx';
+import {useReducer, useRef} from 'react';
 
-// 1. "/" : Home
-// 2. "/new" : New
-// 3. "/diary" : Diary
+const mockData = [
+  {
+    id        :1,
+    createDate:new Date().getTime(),
+    emotionId :1,
+    content   :'first',
+  },
+  {
+    id        :2,
+    createDate:new Date().getTime(),
+    emotionId :2,
+    content   :'second',
+  },
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state, ];
+    case 'UPDATE':
+      return state.map(item =>
+          String(item.id) === String(action.data.id) ? action.data : item,
+      )
+    case 'DELETE':
+      return state.filter(item => String(item.id) !== String(action.data));
+    default:
+      return state;
+  }
+}
+
 function App() {
 
-  let nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
 
-  const onClickButton = () => {
-    nav('/new')
+  // create new diary
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type:'CREATE',
+      data:{
+        id:idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+
+  // edit 
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type:'UPDATE',
+      data:{
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    })
   };
+  
+  const onDelete = (id) => {
+    dispatch({
+      type:'DELETE',
+      data:id,
+    });
+  }
+
   return (
       <>
-        <Header title={'감정일기장'}
-                leftChild={<Button text="<"/>}
-                rightChild={<Button text=">" />}/>
-        <Button text={'버튼'} onClick={() => (
-            console.log('=>(App.jsx:25) ')
-        )}
-        /><Button text={'버튼'} onClick={() => (
-          console.log('=>(App.jsx:25) ')
-      )} type={'POSITIVE'}
-      /><Button text={'버튼'} onClick={() => (
-          console.log('=>(App.jsx:25) ')
-      )} type={'NEGATIVE'}
-      />
+
+        <button onClick={()=>{
+          onCreate(new Date().getTime(), 1, 'test');
+        }}>create test</button>
+
+        <button onClick={()=>{
+          onUpdate(1, new Date().getTime(), 1, 'update');
+        }}>update test</button>
+
+        <button onClick={()=>{
+          onDelete(1);
+        }}>delete test</button>
         <div>
           <Link to={'/'}>Home</Link>
           <Link to={'/new'}>New</Link>
           <Link to={'/diary'}>Diary</Link>
+          <Link to={'/edit'}>Edit</Link>
+
         </div>
-        <button onClick={onClickButton}> go to new page</button>
         <Routes>
           <Route path={'/'} element={<Home/>}></Route>
           <Route path={'/new'} element={<New/>}></Route>
           <Route path={'/diary/:id'} element={<Diary/>}></Route>
+          <Route path={'/edit/:id'} element={<Edit/>}></Route>
           <Route path="*" element={<Notfound/>}></Route>
         </Routes>
       </>
