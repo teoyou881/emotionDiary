a658d06326be4507a650d3dc23d4a7efb800cf2e
Teo_You <teoyou881@gmail.com>
2025-04-17 04:32:10 -0400

    emotionDiary - impl input date

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Header from '../components/Header.jsx';
 import Button from '../components/Button.jsx';
 import Editor from '../components/Editor.jsx';
+import {useNavigate} from 'react-router-dom';
 
 const New = () => {
+
+  const nav = useNavigate();
+
   return (
       <div>
         <Header title={'create new diary'}
-        leftChild={<Button text={'back'} type={'back'} onClick={() => {history.back()}} />}
+        leftChild={<Button text={'back'} type={'back'}
+        onClick={() => {nav(-1)}}
+            // onClick={() => {history.back()}}
+        />}
         />
         <Editor/>
       </div>
